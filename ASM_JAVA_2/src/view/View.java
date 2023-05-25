cac0e0a1c3410fb7aee048254db5d0c1a46ac021
Hoàng Dũng <dungbui8198@gmail.com>
2023-05-25 16:21:32 +0700

    java2

--- ASM_JAVA_2/src/view/View.java
+++ ASM_JAVA_2/src/view/View.java
@@ -937,7 +937,6 @@ public class View extends javax.swing.JFrame {
         //</editor-fold>
         //</editor-fold>
         //</editor-fold>
-
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(() -> {
             new View().setVisible(true);
