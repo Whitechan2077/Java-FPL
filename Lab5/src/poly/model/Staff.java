a9a4966b3bc008bd380096fb031d0500c8b21428
Hoàng Dũng <dungbui8198@gmail.com>
2023-05-27 01:14:33 +0700

    file

---
+++ Lab5/src/poly/model/Staff.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package poly.model;
+
+/**
+ *
+ * @author buidu
+ */
+public class Staff {
+    public String fullname;
+    public double salary;
+
+    public Staff(String fullname, double salary) {
+        this.fullname = fullname;
+        this.salary = salary;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+    
+}
