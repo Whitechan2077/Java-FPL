52952e2c6d49bff4f785726b9670f3da4afd4965
Hoàng Dũng <dungbui8198@gmail.com>
2023-05-27 15:40:42 +0700

    textReader

---
+++ Lab5/src/poly/xtextreader/XTextReader.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package poly.xtextreader;
+
+/**
+ *
+ * @author buidu
+ */
+public class XTextReader {
+    
+}
