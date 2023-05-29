23819d623c83273a66680f08a82afa2237cac612
Hoàng Dũng <dungbui8198@gmail.com>
2023-05-30 00:06:56 +0700

    nah

--- MultiTheard/src/multitheard/Main.java
+++ MultiTheard/src/multitheard/Main.java
@@ -7,23 +7,21 @@ import theard.Thread1;
 import theard.Thread2;
 /**
  *
- * Đây sẽ là cmt đầu tiên của tôi về lý thuyết
- * 1.Có hai cách tạo thread  1 là implement Runable 2 là extends Thread
- * 2 thread nặc danh là thực hiện một thread mà không cần phải tạo class mới
- * 3.
+ * 
  */
 public class Main {
     public static void main(String[] args) {
-        //các thread thông thường
+        //1.Có hai cách tạo thread  1 là implement Runable 2 là extends Thread
         Thread th1 = new Thread(new Thread1());
         Thread th2 = new Thread(new Thread2());
         th1.start();
         th2.start();
-        //thread nặc danh
+        
+        //2.thread nặc danh một thred mà không phải tạo một class mới 
         new Thread(new Runnable() {
             @Override
             public void run() {
-                throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+                System.out.println("Tao dang la thread nặc danh ");
             }
         }).start();
     }
