c3b7bfc28c91e8b9e8836842e724bad300b4b73d
Hoàng Dũng <dungbui8198@gmail.com>
2023-05-30 13:14:33 +0700

    lý thuyết

--- Generic/src/generic/Generic.java
+++ Generic/src/generic/Generic.java
@@ -14,7 +14,15 @@ public class Generic {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       /*
+        Generics chỉ cần hiểu đơn giản là tạo một class interface linh động kiểu dữ liệu
+        quy ước đặt tên generics 
+        T,V,E,K,N
+        Generics không thể có static 
+        không thể tạo mảng với generics
+        */
+        GenericClass generics1 = new GenericClass("Lmao",23);
+        GenericClass<String,Integer> generics2 = new GenericClass<>("lmao",123);
     }
     
 }
