7c11b320b86bb03bad3c5fe46648086a5b4f286d
Hoàng Dũng <dungbui8198@gmail.com>
2023-06-05 23:39:38 +0700

    Update Enum.java

--- ENUMERATIONS, AUTOBOXING,  STATIC IMPORT, AND ANNOTATIONS/src/emum/Enum.java
+++ ENUMERATIONS, AUTOBOXING,  STATIC IMPORT, AND ANNOTATIONS/src/emum/Enum.java
@@ -11,12 +11,45 @@ package emum;
 public class Enum {
     
     public enum Tranport{
-        CAR(65), TRUCK(55), AIRPLANE(600), TRAIN(70), BOAT(22);
+        CAR("lenovo",65), TRUCK("Truck-kin",55),
+        AIRPLANE("May bay ba gia",600), TRAIN("Choi tau hoa",70), 
+        BOAT("Quan lot",22);
         private final int speed;
+        private final String name;
 
-        private Tranport(int speed) {
+        private Tranport(String name, int speed) {
             this.speed = speed;
+            this.name = name;
         }
+
+        public static Tranport getCAR() {
+            return CAR;
+        }
+
+        public static Tranport getTRUCK() {
+            return TRUCK;
+        }
+
+        public static Tranport getAIRPLANE() {
+            return AIRPLANE;
+        }
+
+        public static Tranport getTRAIN() {
+            return TRAIN;
+        }
+
+        public static Tranport getBOAT() {
+            return BOAT;
+        }
+
+        public int getSpeed() {
+            return speed;
+        }
+
+        public String getName() {
+            return name;
+        }
+        
     }
     
     public static void main(String[] args) {
