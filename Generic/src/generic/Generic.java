6ee0ba4b7eaabb717b3dd5debd55394dcac04432
Hoàng Dũng <dungbui8198@gmail.com>
2023-06-07 19:27:10 +0700

    gen

--- Generic/src/generic/Generic.java
+++ Generic/src/generic/Generic.java
@@ -15,16 +15,9 @@ public class Generic {
      */
     public static void main(String[] args) {
         var a ="a";
-        
-       /*
-        Generics chỉ cần hiểu đơn giản là tạo một class interface linh động kiểu dữ liệu
-        quy ước đặt tên generics 
-        T,V,E,K,N
-        Generics không thể có static 
-        không thể tạo mảng với generics        
-        */
         GenericClass generics1 = new GenericClass("Lmao",23);
         GenericClass<String,Integer> generics2 = new GenericClass<>("lmao",123);
+        GenericClass<Integer,String> gen3 = new GenericClass<>(3,"lmao");
     }
     
 }
---
+++ Generic/src/generic/GenericsLimitedDataTuye.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package generic;
+
+/**
+ *
+ * @author buidu
+ * @param <T>
+ */
+public class GenericsLimitedDataTuye <T extends Number>{
+    private T number;
+
+    public GenericsLimitedDataTuye(T number) {
+        this.number = number;
+    }
+
+    public T getNumber() {
+        return number;
+    }
+
+    public void setNumber(T number) {
+        this.number = number;
+    }       
+}
